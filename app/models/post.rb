3e33979d0eedbd5dae2544efe69dae4c720d045e
Kelvin Kimaru <kelvinkimaru@gmail.com>
2023-08-28 00:41:26 +0300

    Post custom validation done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  validate :click_baity
+
+  def click_baity
+    clickbait_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+    if title.nil? || !clickbait_words.any? { |word| title.include?(word) }
+      errors.add(:title, "not clickbait-y enough!")
+    end
+  end
 end
